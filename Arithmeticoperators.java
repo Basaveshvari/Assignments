a1b0bd5875c919073cded2afa2455f8812ad8776
Basaveshwari J <jangabasaveshwari@gmail.com>
2025-05-24 10:57:04 +0530

    Add files via upload

---
+++ Arithmeticoperators.java
@@ -0,0 +1,45 @@
+package dailytasks;
+
+public class Arithmeticoperators {
+
+	public static void main(String[] args) {
+		
+				int a=30;
+				int b=60;
+				int c=a+b;
+				int d=a-b;
+				int e=a*b;
+				int f=a/b;
+				int g=a%b;
+				
+				System.out.println("a=30");
+				System.out.println("b=60");
+				
+				System.out.println("");
+				
+				System.out.println("Addition:" +c);
+				System.out.println("Substraction:" +d);
+				System.out.println("Multiplication:" +e);
+				System.out.println("Division:" +f);
+				System.out.println("Modulus:" +g);
+				
+				
+				System.out.println("\n");
+				
+				System.out.println("--Relational Operators--");
+		        
+				System.out.println("a=40");
+				System.out.println("b=60");
+				
+				System.out.println("");
+				
+				System.out.println("a > b:" +(a>b));
+				System.out.println("a < b:" +(a<b));
+				System.out.println("a == b:" +(a==b));
+				System.out.println("a != b:" +(a!=b));
+				System.out.println("a >= b:" +(a>=b));
+				System.out.println("a <= b:" +(a<=b));
+			}
+
+		}
+		
